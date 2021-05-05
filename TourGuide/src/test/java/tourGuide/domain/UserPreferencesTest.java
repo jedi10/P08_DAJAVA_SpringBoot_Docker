9371bf95bbd908874d4dac0e6f3217a19c4440e8
jedi10 <kyle10_dev@hotmail.fr>
2021-05-05 17:49:04 +0200

    Add domain Unit Test; Fix some Pojo trouble

---
+++ TourGuide/src/test/java/tourGuide/domain/UserPreferencesTest.java
@@ -0,0 +1,23 @@
+package tourGuide.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static pl.pojo.tester.api.FieldPredicate.exclude;
+import static pl.pojo.tester.api.assertion.Assertions.assertPojoMethodsFor;
+import static pl.pojo.tester.api.assertion.Method.*;
+
+class UserPreferencesTest {
+
+    @Test
+    public void userPreferencesPojo(){
+        //GIVEN
+        final Class<?> classUnderTest = UserPreferences.class;
+        //WHEN-THEN
+        assertPojoMethodsFor(classUnderTest)
+                .testing(SETTER)
+                .testing(GETTER)
+                .testing(CONSTRUCTOR)
+                .areWellImplemented();
+    }
+}
