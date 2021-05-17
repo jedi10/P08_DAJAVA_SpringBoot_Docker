f9a720d5b6ee74f730c1159304dba543ba01d29f
jedi10 <kyle10_dev@hotmail.fr>
2021-05-17 17:53:11 +0200

    Add Lombok Library for Pojo

--- TourGuide/src/main/java/tourGuide/domain/UserPreferences.java
+++ TourGuide/src/main/java/tourGuide/domain/UserPreferences.java
@@ -3,11 +3,14 @@ package tourGuide.domain;
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 
+import lombok.*;
 import org.javamoney.moneta.Money;
 
-import java.util.Objects;
-
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class UserPreferences {
 	
 	private int attractionProximity = Integer.MAX_VALUE;
@@ -19,102 +22,4 @@ public class UserPreferences {
 	private int numberOfAdults = 1;
 	private int numberOfChildren = 0;
 
-	public UserPreferences(int attractionProximity, CurrencyUnit currency, Money lowerPricePoint, Money highPricePoint,
-							int tripDuration, int ticketQuantity, int numberOfAdults, int numberOfChildren) {
-		this.attractionProximity = attractionProximity;
-		this.currency = currency;
-		this.lowerPricePoint = lowerPricePoint;
-		this.highPricePoint = highPricePoint;
-		this.tripDuration = tripDuration;
-		this.ticketQuantity = ticketQuantity;
-		this.numberOfAdults = numberOfAdults;
-		this.numberOfChildren = numberOfChildren;
-	}
-
-	public UserPreferences() {
-	}
-
-	public void setAttractionProximity(int attractionProximity) {
-		this.attractionProximity = attractionProximity;
-	}
-	
-	public int getAttractionProximity() {
-		return attractionProximity;
-	}
-
-	public CurrencyUnit getCurrency() {
-		return currency;
-	}
-
-	public void setCurrency(CurrencyUnit currency) {
-		this.currency = currency;
-	}
-
-	public Money getLowerPricePoint() {
-		return lowerPricePoint;
-	}
-
-	public void setLowerPricePoint(Money lowerPricePoint) {
-		this.lowerPricePoint = lowerPricePoint;
-	}
-
-	public Money getHighPricePoint() {
-		return highPricePoint;
-	}
-
-	public void setHighPricePoint(Money highPricePoint) {
-		this.highPricePoint = highPricePoint;
-	}
-	
-	public int getTripDuration() {
-		return tripDuration;
-	}
-
-	public void setTripDuration(int tripDuration) {
-		this.tripDuration = tripDuration;
-	}
-
-	public int getTicketQuantity() {
-		return ticketQuantity;
-	}
-
-	public void setTicketQuantity(int ticketQuantity) {
-		this.ticketQuantity = ticketQuantity;
-	}
-	
-	public int getNumberOfAdults() {
-		return numberOfAdults;
-	}
-
-	public void setNumberOfAdults(int numberOfAdults) {
-		this.numberOfAdults = numberOfAdults;
-	}
-
-	public int getNumberOfChildren() {
-		return numberOfChildren;
-	}
-
-	public void setNumberOfChildren(int numberOfChildren) {
-		this.numberOfChildren = numberOfChildren;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (!(o instanceof UserPreferences)) return false;
-		UserPreferences that = (UserPreferences) o;
-		return attractionProximity == that.attractionProximity &&
-				tripDuration == that.tripDuration &&
-				ticketQuantity == that.ticketQuantity &&
-				numberOfAdults == that.numberOfAdults &&
-				numberOfChildren == that.numberOfChildren &&
-				currency.equals(that.currency) &&
-				lowerPricePoint.equals(that.lowerPricePoint) &&
-				highPricePoint.equals(that.highPricePoint);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(attractionProximity, currency, lowerPricePoint, highPricePoint, tripDuration, ticketQuantity, numberOfAdults, numberOfChildren);
-	}
 }
--- TourGuide/src/main/java/tourGuide/domain/UserReward.java
+++ TourGuide/src/main/java/tourGuide/domain/UserReward.java
@@ -2,38 +2,23 @@ package tourGuide.domain;
 
 import gpsUtil.location.Attraction;
 import gpsUtil.location.VisitedLocation;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class UserReward {
 
 	public final VisitedLocation visitedLocation;
 	public final Attraction attraction;
 	private int rewardPoints;
-	public UserReward(VisitedLocation visitedLocation, Attraction attraction, int rewardPoints) {
-		this.visitedLocation = visitedLocation;
-		this.attraction = attraction;
-		this.rewardPoints = rewardPoints;
-	}
 	
 	public UserReward(VisitedLocation visitedLocation, Attraction attraction) {
 		this.visitedLocation = visitedLocation;
 		this.attraction = attraction;
 	}
-
-	public VisitedLocation getVisitedLocation() {
-		return visitedLocation;
-	}
-
-	public Attraction getAttraction() {
-		return attraction;
-	}
-
-	public void setRewardPoints(int rewardPoints) {
-		this.rewardPoints = rewardPoints;
-	}
-	
-	public int getRewardPoints() {
-		return rewardPoints;
-	}
-
 
 }
