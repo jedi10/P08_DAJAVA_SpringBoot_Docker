c3baf6de3ea1b8c9011a02c7a2ffc2d8ad087a6d
jedi10 <kyle10_dev@hotmail.fr>
2021-05-28 17:25:36 +0200

    GPSUtils project: add Controller + E2E Test
    Pojo for Test has been added because Jackson can't use Jar Library Pojo

---
+++ GPSUtils/src/main/java/com/tourguide/gpsutils/controller/GpsUtilsController.java
@@ -0,0 +1,48 @@
+package com.tourguide.gpsutils.controller;
+
+import com.tourguide.gpsutils.service.IGpsUtilsService;
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * <b>GPSUtils Microservice EndPoints</b>
+ */
+@RestController
+public class GpsUtilsController {
+    private static final Logger logger = LogManager.getLogger(GpsUtilsController.class);
+
+    @Autowired
+    private IGpsUtilsService gpsUtilsService;
+
+    /**
+     * <b>getUserLocation for a given user</b>
+     * @param userId mandatory userId
+     * @return get VisitedLocation for given user
+     */
+    @GetMapping("/userLocation")
+    public VisitedLocation getUserLocation(@RequestParam UUID userId)
+    {
+        logger.debug("Call to gpsUtils.getUSerLocation({})", userId );
+        return gpsUtilsService.getUserLocation(userId);
+    }
+
+    /**
+     * <b>get list of all Attractions</b>
+     * @return list of attractions
+     */
+    @GetMapping("/attractions")
+    public List<Attraction> getAttractions()
+    {
+        logger.debug("Call to gpsUtils.getAttractions()");
+        return gpsUtilsService.getAttractions();
+    }
+}
