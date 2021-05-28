52d3ca5388b3c6dea4aec20ca87d01727fffabcd
jedi10 <kyle10_dev@hotmail.fr>
2021-05-28 14:44:40 +0200

    GPSUtils project: add service and Integration Test

---
+++ GPSUtils/src/main/java/com/tourguide/gpsutils/service/GpsUtilsServiceImpl.java
@@ -0,0 +1,51 @@
+package com.tourguide.gpsutils.service;
+
+import gpsUtil.GpsUtil;
+import gpsUtil.location.Attraction;
+import gpsUtil.location.VisitedLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
+
+/**
+ * <b>Interaction with gpsUtil external libs</b>
+ * <p>gpsUtil.jar has been added to project as an external library</p>
+ */
+@Service
+public class GpsUtilsServiceImpl implements IGpsUtilsService {
+
+    private static final Logger logger = LogManager.getLogger(GpsUtilsServiceImpl.class);
+
+    /**
+     * <b>External Libs incorporated in project as Jar</b>
+     */
+    private GpsUtil gpsUtil;
+
+    /**
+     * Default Constructor
+     */
+    @Autowired
+    public GpsUtilsServiceImpl(GpsUtil gpsUtil)
+    {
+        Locale.setDefault(Locale.US);
+        this.gpsUtil = gpsUtil;
+    }
+
+
+    @Override
+    public VisitedLocation getUserLocation(UUID userId) {
+        logger.debug("Call to gpsUtil.getUSerLocation(" + userId + ")");
+        return gpsUtil.getUserLocation(userId);
+    }
+
+    @Override
+    public List<Attraction> getAttractions() {
+        logger.debug("Call to gpsUtil.getAttractions()");
+        return gpsUtil.getAttractions();
+    }
+}
