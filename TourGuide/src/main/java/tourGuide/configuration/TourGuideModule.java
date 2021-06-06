d27620ef73dc26243f78b9f826212e667f3707d8
jedi10 <kyle10_dev@hotmail.fr>
2021-06-06 14:09:09 +0200

    Move TourGuideModule in configuration package

--- TourGuide/src/main/java/tourGuide/configuration/TourGuideModule.java
+++ TourGuide/src/main/java/tourGuide/configuration/TourGuideModule.java
@@ -1,4 +1,4 @@
-package tourGuide;
+package tourGuide.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
---
+++ TourGuide/src/main/java/tourGuide/configuration/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <b>Application configuration</b>
+ */
+package tourGuide.configuration;
