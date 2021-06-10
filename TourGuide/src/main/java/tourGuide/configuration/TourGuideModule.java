1a23eb6ce1f8b7f720f01ec6b12ed5371371740f
jedi10 <kyle10_dev@hotmail.fr>
2021-06-10 17:06:46 +0200

    Add a Bean for RestTemplate (Test purpose)

--- TourGuide/src/main/java/tourGuide/configuration/TourGuideModule.java
+++ TourGuide/src/main/java/tourGuide/configuration/TourGuideModule.java
@@ -1,11 +1,13 @@
 package tourGuide.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 import tourGuide.configuration.MicroserviceProperties;
 import tourGuide.service.restTemplateService.GpsUtilRestService;
@@ -34,8 +36,17 @@ public class TourGuideModule {
 		return new RewardCentral();
 	}
 
-	@Bean
+	@Bean(name="defaultRest")
+	@Primary
 	public RestTemplate requestRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder
+				.setConnectTimeout(Duration.ofSeconds(2))
+				.setReadTimeout(Duration.ofSeconds(2))
+				.build();
+	}
+
+	@Bean(name="requestRestTemplate2")
+	public RestTemplate requestRestTemplate2(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder
 				.setConnectTimeout(Duration.ofSeconds(2))
 				.setReadTimeout(Duration.ofSeconds(2))
@@ -48,3 +59,6 @@ public class TourGuideModule {
 	}
 
 }
+
+
+//https://www.baeldung.com/spring-qualifier-annotation
